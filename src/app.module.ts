95751363b5e5bc6d5b8802067e64b566f241c72d
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-11 23:23:32 -0300

    feat: add Mailer resource module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailerModule } from './mailer/mailer.module';
 
 @Module({
-  imports: [],
+  imports: [MailerModule],
   controllers: [AppController],
   providers: [AppService],
 })
