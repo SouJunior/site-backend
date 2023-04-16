2b8b8b487adb9c3978f3efb16792f18176deb213
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-16 11:05:39 -0300

    chore: enable ConfigModule globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MailModule } from './shared/providers/mailer/mailer.module';
-import { DonatorModule } from './modules/donators/donator.module';
+import { SponsorModule } from './modules/sponsor/sponsor.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MailModule, DonatorModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MailModule,
+    SponsorModule
+  ],
   providers: [],
 })
 export class AppModule { }
