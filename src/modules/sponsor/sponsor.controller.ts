fda22a2d27e288e73d9cb09032d4f3739a371c5a
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-04-18 09:00:22 -0300

    feat: add swagger

--- src/modules/sponsor/sponsor.controller.ts
+++ src/modules/sponsor/sponsor.controller.ts
@@ -1,11 +1,23 @@
-import { Body, Controller, HttpCode, Post, Res } from '@nestjs/common';
+import { Body, Controller, HttpCode, Injectable, Post, Res } from '@nestjs/common';
 import { SendMailToNewSponsorUseCase } from './use-cases/send-mail-to-new-donator';
 import { SendMailDto } from '../../shared/providers/mailer/dto/send-mail-dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('sponsor')
 export class SponsorController {
   constructor(private readonly sendMailToNewSponsorUseCase: SendMailToNewSponsorUseCase) { }
 
+  @ApiOperation({
+    summary: 'Send email to admin about new sponsor',
+  })
+  @ApiResponse({
+    status: 200
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Error',
+    type: 'string'
+  })
   @Post()
   @HttpCode(200)
   async send(@Body() sendMailData: SendMailDto) {
