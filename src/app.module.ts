dd3bfaa3419dc62afab62c98410d7e1256a61222
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-05-20 22:01:21 -0300

    chore: move folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MailModule } from './shared/providers/mailer/mailer.module';
 import { SponsorModule } from './modules/sponsor/sponsor.module';
 import { ConfigModule } from '@nestjs/config';
-import { CollaboratorsModule } from './collaborators/collaborators.module';
+import { CollaboratorsModule } from './modules/collaborators/collaborators.module';
 
 @Module({
   imports: [
