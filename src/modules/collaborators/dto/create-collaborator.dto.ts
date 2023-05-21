e969535f89d45bec3dbee90d14ea02892dce896d
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-05-20 22:03:00 -0300

    feat: add collaborator DTO

--- src/modules/collaborators/dto/create-collaborator.dto.ts
+++ src/modules/collaborators/dto/create-collaborator.dto.ts
@@ -1,1 +1,14 @@
-export class CreateCollaboratorDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCollaboratorDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  role: string;
+}
