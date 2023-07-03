91ee9469cfc8ebcbcd2c38722664c76def182049
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2023-07-03 20:43:15 -0300

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('SouJunior website')
