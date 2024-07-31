4f32cd6c3c61694b5b67ca25fda8d012dbfb2040
Guilherme Henrique <guilhermehoxz@gmail.com>
2024-07-31 20:24:24 -0300

    correção no controller

--- src/modules/area/area.controller.ts
+++ src/modules/area/area.controller.ts
@@ -9,7 +9,7 @@ export class AreaController {
 @Get()
 async findAll(): Promise<AreaDto[]> {
   try {
-  const areas = this.areaService.getAreasWithSubareas();
+  const areas = await this.areaService.getAreasWithSubareas();
   return areas;
   } catch (error) { 
     throw new HttpException(
