7daab00862bcfae3d2308039b0b90d023f0258fe
Kaique <kaiquesantiagop@gmail.com>
2025-02-24 19:50:21 -0300

    Atualizada estrrutura de apoiador

--- src/database/entities/supporter.mongo-entity.ts
+++ src/database/entities/supporter.mongo-entity.ts
@@ -1,7 +1,7 @@
 import { Entity, ObjectIdColumn, ObjectId, Column } from 'typeorm';
 
 @Entity('Supporter')
-export class SupporterEntity{
+export class SupporterEntity {
     @ObjectIdColumn()
     id: ObjectId;
 
@@ -11,16 +11,45 @@ export class SupporterEntity{
     @Column({ length: 255 })
     name: string;
 
-    @Column({length: 255})
-    surname: string;
+    @Column({ length: 255, nullable: true })
+    institution?: string;
 
-    @Column({length: 255})
+    @Column({ length: 255 })
     phone: string;
 
-    @Column({ length: 255})
+    @Column({ length: 255 })
     email: string;
 
-    @Column({ length: 500})
-    message: string;
+    @Column()
+    hasWhatsApp: boolean;
 
+    @Column({ length: 255 })
+    supportType: string;
+
+    @Column({ length: 500, nullable: true })
+    linksForSupport?: string;
+
+    @Column({ length: 500, nullable: true })
+    expertiseAreas?: string;
+
+    @Column({ length: 500, nullable: true })
+    opportunitiesDescription?: string;
+
+    @Column({ length: 500, nullable: true })
+    otherSupportWays?: string;
+
+    @Column({ length: 500 })
+    suggestion: string;
+
+    @Column({ length: 500 })
+    motivation: string;
+
+    @Column({ default: true })
+    agreesWithTerms: boolean;
+
+    @Column({ default: true })
+    agreesWithNonProfit: boolean;
+
+    @Column({ default: true })
+    agreesWithContact: boolean;
 }
