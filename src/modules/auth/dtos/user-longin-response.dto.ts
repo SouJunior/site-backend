95db06a3e6d07129790cd07e6625c5c025d3c83a
Kaique <kaiquesantiagop@gmail.com>
2025-05-01 17:22:06 -0300

    fixed leak

--- src/modules/auth/dtos/user-longin-response.dto.ts
+++ src/modules/auth/dtos/user-longin-response.dto.ts
@@ -4,7 +4,7 @@ import { ApiProperty } from "@nestjs/swagger";
 export class UserLoginResponseDto{
     @ApiProperty({
         example:
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBlZHJpbmhvc2VuYWRvcjI3QGdtYWlsLmNvbSIsImlhdCI6MTc0NjEyMTk0NiwiZXhwIjoxNzQ2MjA4MzQ2fQ.UkQz3vtkAf-QVzWXpSi-cjJjc4QgwhbTljQjvcaSNSw',
+            'auNfzLZvfNxamyftJZpkXsalCneJ82j4juNfzLZvfNxamyftJZpkXsalCneJ82j4juNfzLZvfNxamyftJZpkXsalCneJ82j4juNfzLZvfNxamyftJZpkXsalCneJ82j4j',
         description: "Token de autenticação JWT"
     })
     token: string;
