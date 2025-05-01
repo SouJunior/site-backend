4354d243c1c997c7c867291d01d4be2f455e6b04
Kaique <kaiquesantiagop@gmail.com>
2025-05-01 17:23:49 -0300

    fix: melhoria exemplo token

--- src/modules/auth/dtos/user-longin-response.dto.ts
+++ src/modules/auth/dtos/user-longin-response.dto.ts
@@ -4,7 +4,7 @@ import { ApiProperty } from "@nestjs/swagger";
 export class UserLoginResponseDto{
     @ApiProperty({
         example:
-            'auNfzLZvfNxamyftJZpkXsalCneJ82j4juNfzLZvfNxamyftJZpkXsalCneJ82j4juNfzLZvfNxamyftJZpkXsalCneJ82j4juNfzLZvfNxamyftJZpkXsalCneJ82j4j',
+            'auNfzLZvfNxamyftJZpkXsalCneJ82j4juNfzLZvfNxamyftJZpkXsalCneJ82j4juNfzLZvfNxamyftJZpkXsalCneJ82j4juNfzLZvfNxamyftJZpkXsalCneJ82j4j.exemplo',
         description: "Token de autenticação JWT"
     })
     token: string;
