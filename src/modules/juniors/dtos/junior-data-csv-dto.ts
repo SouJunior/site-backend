22eccbb45e0ea0a4e918a09cd805c55531651e63
Kaique <kaiquesantiagop@gmail.com>
2025-06-03 16:31:08 -0300

    feat: areas e subareas no csv agora mostram os nomes ao invés de seus ids

--- src/modules/juniors/dtos/junior-data-csv-dto.ts
+++ src/modules/juniors/dtos/junior-data-csv-dto.ts
@@ -20,11 +20,11 @@ export class JuniorDataCsvDto{
       subarea ?: string;
 
 
-      constructor(id:ObjectId, name: string, email: string, linkendin: string, area: string, subarea: string){
+      constructor(id:ObjectId, name: string, email: string, linkedin: string, area: string, subarea: string){
             this.id = id;
             this.name = name;
             this.email = email;
-            this.linkedin = linkendin;
+            this.linkedin = linkedin;
             this.area = area;
             this.subarea = subarea;
       }
